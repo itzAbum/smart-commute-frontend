69075808df962271a9c9b274c5f27fc6b7ea08b5
Aahmed <155355389+itzAbum@users.noreply.github.com>
2026-04-14 17:08:53 -0400

    Update settings.py

--- smartcommute/settings.py
+++ smartcommute/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'smartcommute.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'PORT': os.environ.get('MYSQLPORT'),
     }
 }
 
